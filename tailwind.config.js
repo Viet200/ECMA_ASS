15d2deb726acd0044b8fc3cf7fedcee2a104b738
vietnvph13644 <vietnvph13644@fpt.edu.vn>
2022-02-23 15:40:35 +0700

    cai moi truong eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["*.html", "./src/**/*.{html,js}"],
-  theme: {
-      extend: {},
-  },
-  plugins: [],
+    content: ["*.html", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
